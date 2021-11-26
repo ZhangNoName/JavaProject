56ed640ce3e4fcd722e4e6849fd8af43372a4f20
Zhang by sxd <1623190186@qq.com>
2021-11-26 09:49:34 +0800

    初始化

---
+++ src/main/java/com/example/bootopen/BootoenApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootopen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootoenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootoenApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/bootopen/com/HelloSpringBootController.java
@@ -0,0 +1,13 @@
+package com.example.bootopen.com;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloSpringBootController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot";
+    }
+}
